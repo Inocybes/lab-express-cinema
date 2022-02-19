77e8e67480a03b3661f4b90f95e53c8efb0e362e
Eduard Cabrero <eduardc.herrero@gmail.com>
2022-02-19 10:51:08 +0100

    Structure done

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,44 @@
 const express = require('express');
 const router = express.Router();
 
+const Movie = require("../models/Movie.model")
+
 /* GET home page */
 router.get('/', (req, res, next) => res.render('index'));
+
+
+router.get("/movies", (req, res, next) =>{
+
+  // Find Movies on db
+  
+  Movie.find()
+  .then((response) =>{
+    console.log(response);
+    res.render("movies.hbs", {response})
+
+  })
+  .catch((err) =>{
+  
+  })
+   
+})
+
+router.get("/movies/:id", (req, res, next) => {
+
+  // Movie details
+  Movie.findById(req.params.id)
+  .then((response) => {
+    console.log(response)
+    res.render("movie-details.hbs", {response})
+  })
+      
+  .catch((err) =>{
+    console.log(err);
+  })
+  
+})
+
+
+
 
 module.exports = router;
